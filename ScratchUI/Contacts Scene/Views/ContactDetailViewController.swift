b328ac70d33550875d14e28f780652c4329b1eb5
Ozmin Vazquez <ozxmin@me.com>
2024-09-21 23:56:10 -0600

    Contact detail work

--- ScratchUI/Contacts Scene/Views/ContactDetailViewController.swift
+++ ScratchUI/Contacts Scene/Views/ContactDetailViewController.swift
@@ -13,12 +13,28 @@ final class ContactDetailViewController: UIViewController {
     @IBOutlet weak var firstName: UILabel!
     @IBOutlet weak var lastName: UILabel!
 
+    @IBOutlet var firstNames: [UILabel]!
     var contact: ContactEntity?
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.style = .editor
+        navigationItem.title = "Test"
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstName.text = contact?.name
+        firstNames.forEach { $0.text = contact?.name}
         lastName.text = contact?.lastName
+
+
+
     }
 }
-
