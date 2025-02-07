0962da4c450ccb868b43c92210f645d55e01de64
Ozmin Vazquez <ozxmin@me.com>
2025-02-06 20:07:38 -0600

    Refactor helper methods

--- ScratchUI/Support Files/App Files/SceneDelegate.swift
+++ ScratchUI/Support Files/App Files/SceneDelegate.swift
@@ -16,28 +16,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        configWindow(scene)
-        concreteMenuCoordinator()
+        let navigationVC = InitialViewController()
+        configWindow(scene, withRoot: navigationVC)
+        appCoordinator = concreteMenuCoordinator(withNavigator: navigationVC)
+
     }
 }
 
 
 extension SceneDelegate {
-    func configWindow(_ scene: UIScene) {
+    func configWindow(_ scene: UIScene, withRoot root: UIViewController) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.frame = UIScreen.main.bounds
         window?.makeKeyAndVisible()
         window?.backgroundColor = .cyan
+        window?.rootViewController = root
     }
 
-    func concreteMenuCoordinator() {
-        let navigationVC = InitialViewController()
-        window?.rootViewController = navigationVC
-
-        let coordinator = MenuCoordinator(navigator: navigationVC)
+    func concreteMenuCoordinator(withNavigator navigator: UINavigationController) -> some CoordinatorProtocol {
+        let coordinator = MenuCoordinator(navigator: navigator)
         coordinator.wire()
         coordinator.start()
-        self.appCoordinator = coordinator
+        return coordinator
     }
 }
