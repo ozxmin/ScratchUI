a7fa78a5b66a2a52fee2ac4feb094ca56bcf76a1
Ozmin Vazquez <ozxmin@me.com>
2025-02-18 15:43:58 -0600

    WIP Testing concrete Lists (aka Manifests)

--- ScratchUI/App/Coordinator.swift
+++ ScratchUI/App/Coordinator.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 protocol CoordinatorProtocol {
     associatedtype Screen where Screen == Self.Screen
@@ -38,4 +39,84 @@ class Manifest<V, each T> {
     init(wirings elements: @escaping () -> Components) {
         self.elements = elements
     }
+}
+
+
+protocol SceneProtocol<Screen> {
+    associatedtype Screen
+    associatedtype Dependencies
+    typealias Scene = (Screen, Dependencies)
+    var wiring: Scene { get }
+}
+
+final class SceneCoordinator<Scene: SceneProtocol> {
+    var parentCoordinator: SceneCoordinator?
+    var childCoordinator: [SceneCoordinator]?
+    let scene: Scene
+    lazy var screen: Scene.Screen = { scene.wiring.0 }()
+
+    init(scene: Scene) {
+        self.scene = scene
+    }
+    func navigate(to coordinator: SceneCoordinator) {
+
+    }
+}
+
+extension SceneCoordinator where Scene.Screen: UIViewController {
+    func start() {
+        parentCoordinator?.navigate(to: self)
+    }
+}
+
+extension SceneCoordinator where Scene.Screen == RootNavigationController {
+    func start() {
+    }
+    func navigate<T>(to coordinator: SceneCoordinator<T>) where T.Screen: UIViewController {
+        if parentCoordinator == nil {
+            screen.setViewControllers([coordinator.screen], animated: false)
+        }
+    }
+}
+
+extension SceneCoordinator {
+    func rootNavigation(root: UINavigationController) {
+
+    }
+}
+
+extension MenuFlows {
+    static func routing(flow: MenuFlows) -> any SceneProtocol.Type {
+        switch flow {
+            case .contacts:
+                return MenuList<MenuTableViewController>.self
+            default: return ContactsList.self
+        }
+    }
+}
+
+
+final class MenuList<Screen> where Screen: MenuViewInterface {
+    typealias Dependencies = (MenuPresenterInterface, MenuInteractorInterface, MenuDataManagerProtocol)
+}
+extension MenuList: SceneProtocol where Screen == MenuTableViewController {
+    var wiring:  Scene {
+        let dm = MenuDataManager()
+        let interactor = MenuInteractor()
+        let presenter = MenuPresenter()
+        let vc = MenuTableViewController(presenter: presenter)
+        interactor.dm = dm
+        presenter.view = vc
+        presenter.interactor = interactor
+        return (vc, (presenter, interactor, dm))
+    }
+
+}
+
+class ContactsList: SceneProtocol {
+    var wiring: Scene {
+        return (ContactsTableViewController(), ContactsPresenter())
+    }
+    typealias Screen = ContactsViewProtocol
+    typealias Dependencies = ContactsPresenterProtocol
 }
